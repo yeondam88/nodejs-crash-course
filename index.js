e321356c3ba3a452781c9bf81f0716285bf7836a
Yeondam Park <giyen9088@gmail.com>
2018-12-11 22:45:39 -0800

    adding template to display html markup

--- index.js
+++ index.js
@@ -9,9 +9,31 @@ const server = http.createServer((req, res) => {
   const pathname = url.parse(req.url, true).pathname;
   const id = url.parse(req.url, true).query.id;
 
+  // PRODUCT OVERVIEW
   if (pathname === "/products" || pathname === "/") {
     res.writeHead(200, { "Conten-type": "text/html" });
-    res.end("This is the products page");
+
+    fs.readFile(
+      `${__dirname}/templates/template-overview.html`,
+      "utf-8",
+      (err, data) => {
+        let overviewOutput = data;
+        fs.readFile(
+          `${__dirname}/templates/template-card.html`,
+          "utf-8",
+          (err, data) => {
+            const cardsOutput = laptopData
+              .map(el => replaceTemplate(data, el))
+              .join("");
+            overviewOutput = overviewOutput.replace("{%CARDS%}", cardsOutput);
+
+            res.end(overviewOutput);
+          }
+        );
+      }
+    );
+
+    // LAPTOP DETAIL
   } else if (pathname === "/laptop" && id < laptopData.length) {
     res.writeHead(200, {
       "Content-type": "text/html"
@@ -21,17 +43,12 @@ const server = http.createServer((req, res) => {
       "utf-8",
       (err, data) => {
         const laptop = laptopData[id];
-        let output = data.replace(/{%PRODUCTNAME%}/g, laptop.productName);
-        output = output.replace(/{%IMAGE%}/g, laptop.image);
-        output = output.replace(/${PRICE}/g, laptop.price);
-        output = output.replace(/{%SCREEN%}/g, laptop.screen);
-        output = output.replace(/{%CPU}/g, laptop.cpu);
-        output = ouuput.replace(/{%STORAGE%}/g, laptop.storage);
-        output = output.replace(/{%RAM%}/g, laptop.ram);
-        output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+        const output = replaceTemplate(data, laptop);
         res.end(output);
       }
     );
+
+    // URL NOT FOUND
   } else {
     res.writeHead(404, { "Content-type": "text/html" });
     res.end("URL was not found on the server!");
@@ -41,3 +58,16 @@ const server = http.createServer((req, res) => {
 server.listen(1337, "127.0.0.1", () => {
   console.log("Server is now on port 1337");
 });
+
+function replaceTemplate(originalHtml, laptop) {
+  let output = originalHtml.replace(/{%PRODUCTNAME%}/g, laptop.productName);
+  output = output.replace(/{%IMAGE%}/g, laptop.image);
+  output = output.replace(/{%PRICE%}/g, laptop.price);
+  output = output.replace(/{%SCREEN%}/g, laptop.screen);
+  output = output.replace(/{%CPU%}/g, laptop.cpu);
+  output = output.replace(/{%STORAGE%}/g, laptop.storage);
+  output = output.replace(/{%RAM%}/g, laptop.ram);
+  output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+  output = output.replace(/{%ID%}/g, laptop.id);
+  return output;
+}
